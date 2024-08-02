0c763b225f603325e6f1469f5c1dbd2ff62a1125
aivaneezy1 <aivaneezydakii@gmail.com>
2024-08-02 19:48:47 +0200

    fixed vercel error pt1

--- app/editpost/page.jsx
+++ app/editpost/page.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import EditPost from '../Components/EditPost'
-import AlertDelete from '../utils/AlertDelete'
+import React, { Suspense } from 'react';
+import EditPost from './EditPost'; // adjust the import path as needed
 
-const Editpage = () => {
+const EditPostPage = () => {
   return (
     <div className='mt-10 ml-5'>
       <h2 className='head_text text-left text-secondary'>Edit Post</h2>
       <h2 className='text-gray-500 text-left mt-5 '>Edit your post however you want.</h2>
-      <EditPost/>
+      <Suspense fallback={<div>Loading...</div>}>
+        <EditPost />
+      </Suspense>
     </div>
-  )
-}
+  );
+};
 
-export default Editpage
+export default EditPostPage;
--- app/profile/[user]/page.jsx
+++ app/profile/[user]/page.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useEffect, useState } from "react";
+import React, { Suspense } from 'react';
 import UserProfile from "@/app/Components/UserProfile";
 import { useSearchParams } from "next/navigation";
 
@@ -10,7 +10,9 @@ const Userpage =  () => {
     <div className="mt-10 ml-5">
       <h2 className="head_text text-left text-secondary">{user} Profile</h2>
       <h2 className="text-gray-500 text-left mt-5">View your own post.</h2>
+    <Suspense fallback={<div>Loading...</div>}>
     <UserProfile/>
+    </Suspense>
     </div>
   );
 };
